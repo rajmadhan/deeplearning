f0129e6de95d01111f5ca358dc2363d418750431
Rajmadhan Ekambaram <erajmadhan@gmail.com>
2024-09-25 18:31:09 -0700

    put the test code inside __main__

--- gpt/gpt-02/model.py
+++ gpt/gpt-02/model.py
@@ -152,63 +152,65 @@ class GPT(nn.Module):
 
         return model
 
-device = "cpu"
-if torch.cuda.is_available():
-    device = "cuda"
-
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-text = open('gpt/tiny-shakespeare.txt', 'r', encoding='utf-8').read()
-print('dataset len: ', len(text))
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1], device=device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
-
-#model = GPT.from_pretrained('gpt2')
-model = GPT(GPTConfig())
-print('all good')
-model.to(device)
-
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-for i in range(50):
-    optimizer.zero_grad()
-    logits, loss = model(x, y)
-    loss.backward()
-    optimizer.step()
-    print(f"step: {i}, loss: {loss.item(): .6f}")
-
-print(loss)
-import sys; sys.exit(0)
-
-model.eval()
-model.to(device)
-
-num_return_sequences = 5
-max_length = 30
-
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to(device)
-
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-while x.size(1) < max_length:
-    with torch.no_grad():
-        logits = model(x)
-        logits = logits[:, -1, :]
-        probs = F.softmax(logits, dim=1)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-        ix = torch.multinomial(topk_probs, 1)
-        xcol = torch.gather(topk_indices, -1, ix)
-        x = torch.cat((x, xcol), dim=1)
-
-for i in range(num_return_sequences):
-    tokens = x[i,:max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+if __name__ == "__main__":
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    
+    import tiktoken
+    enc = tiktoken.get_encoding('gpt2')
+    text = open('gpt/tiny-shakespeare.txt', 'r', encoding='utf-8').read()
+    print('dataset len: ', len(text))
+    text = text[:1000]
+    tokens = enc.encode(text)
+    B, T = 4, 32
+    buf = torch.tensor(tokens[:B*T + 1], device=device)
+    x = buf[:-1].view(B, T)
+    y = buf[1:].view(B, T)
+    
+    #model = GPT.from_pretrained('gpt2')
+    model = GPT(GPTConfig())
+    print('all good')
+    model.to(device)
+    
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    for i in range(50):
+        optimizer.zero_grad()
+        logits, loss = model(x, y)
+        loss.backward()
+        optimizer.step()
+        print(f"step: {i}, loss: {loss.item(): .6f}")
+    
+    print(loss)
+    import sys; sys.exit(0)
+    
+    model.eval()
+    model.to(device)
+    
+    num_return_sequences = 5
+    max_length = 30
+    
+    import tiktoken
+    enc = tiktoken.get_encoding('gpt2')
+    tokens = enc.encode("Hello, I'm a language model,")
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+    x = tokens.to(device)
+    
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    while x.size(1) < max_length:
+        with torch.no_grad():
+            logits = model(x)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=1)
+            topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+            ix = torch.multinomial(topk_probs, 1)
+            xcol = torch.gather(topk_indices, -1, ix)
+            x = torch.cat((x, xcol), dim=1)
+    
+    for i in range(num_return_sequences):
+        tokens = x[i,:max_length].tolist()
+        decoded = enc.decode(tokens)
+        print(">", decoded)
+    
