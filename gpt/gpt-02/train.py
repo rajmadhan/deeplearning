54a517ad5d0d64275b8dd45033b677ee52725451
Rajmadhan Ekambaram <erajmadhan@gmail.com>
2024-09-26 18:50:11 -0700

    changed block and batch size, optimizer parameters and added gradient norm clip

--- gpt/gpt-02/train.py
+++ gpt/gpt-02/train.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         device = "cuda"
     torch.set_float32_matmul_precision("high")
 
-    B, T = 32, 128
+    B, T = 8, 1024
     dataloader = DataLoaderLite("gpt/tiny-shakespeare.txt", B, T)
     #model = GPT.from_pretrained('gpt2')
     model = GPT(GPTConfig(block_size=T))
@@ -24,10 +24,10 @@ if __name__ == "__main__":
     td = (t1 - t0)*1000
     print(f"model compile time: {td: .2f}ms")
     
-    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
 
     start_time = time.time()
-    for i in range(500):
+    for i in range(100):
         t0 = time.time()
         optimizer.zero_grad()
         x, y = dataloader.next_batch()
@@ -35,13 +35,14 @@ if __name__ == "__main__":
         with torch.autocast(device_type=device, dtype=torch.bfloat16):
             logits, loss = model(x, y)
         loss.backward()
+        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         torch.cuda.synchronize()
         t1 = time.time()
         td = (t1 - t0)*1000
         tokens_per_sec = (dataloader.B * dataloader.T) / (t1 - t0)
-        print(f"step: {i}, loss: {loss.item(): .6f}, td: {td: .2f}ms, tok/sec: {tokens_per_sec:.2f}")
-        if i and not i%100:
+        print(f"step: {i} | loss: {loss.item(): .6f} | norm: {norm: .4f} | td: {td: .2f}ms | tok/sec: {tokens_per_sec:.2f}")
+        if not i%100:
             end_time = time.time()
             elapsed_time = end_time - start_time
             print(f"elapsed time: {elapsed_time: .3f}")
